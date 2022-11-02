e90acea4893b5317c6c260980a318fca12d713b3
Alex Cimpan <alex.cimpan0@yahoo.com>
2022-11-02 10:50:31 +0100

    Alex

---
+++ Domain/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public string AuthorId { get; set; }
+    public ApplicationUser Author { get; set; }
+    public ICollection<Comment> Comments { get; set; }
+}
---
+++ Domain/Models/SubPost.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Models;
+
+public class SubPost
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public User Owner { get; set; }
+    
+}
