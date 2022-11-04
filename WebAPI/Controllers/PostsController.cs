15aaa3379266370c5e3b5f8d76bd78f58f1bce98
Bhupas <bhupas@outlook.com>
2022-11-04 09:05:18 +0100

    Create PostsController.cs

---
+++ WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,85 @@
+﻿using Application.Logic;
+using Application.LogicInterfaces;
+using Domain;
+using Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PostsController : ControllerBase
+{
+    private readonly IPostLogic postLogic;
+
+    public PostsController(IPostLogic postLogic)
+    {
+        this.postLogic = postLogic;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<Post>> CreateAsync(PostCreationDto postCreationDto)
+    {
+        try
+        {
+            Post post = await postLogic.CreateAsync(postCreationDto);
+            return Created($"/posts/{post.Id}", post);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        } 
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Post>>> GetAsync([FromQuery] string? subPost)
+    {
+        try
+        {
+            IEnumerable<Post> posts = await postLogic.GetAsync(subPost);
+            return Ok(posts);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpGet]
+    [Route("/Post/")]
+    public async Task<ActionResult<Post?>> GetByIdAsync([FromQuery] int id)
+    {
+        try
+        {
+            Console.WriteLine(id);
+            Post? post = await postLogic.GetByIdAsync(id);
+            return Ok(post);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteAsync([FromQuery] int id)
+    {
+        try
+        {
+            await postLogic.DeleteAsync(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+}
