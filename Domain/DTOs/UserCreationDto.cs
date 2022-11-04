e8c61ed3f19c883174bc8250c270e816819f303f
Bhupas <bhupas@outlook.com>
2022-11-04 08:59:28 +0100

    Added Postcontroller
    Added Postcontroller

--- Domain/DTOs/UserCreationDto.cs
+++ Domain/DTOs/UserCreationDto.cs
@@ -7,7 +7,7 @@ namespace Shared.DTOs;
 public class UserCreationDto
 {
     public string UserName { get;}
-    //public string Password { get;}
+    public string Password { get;}
 
     public UserCreationDto(string userName/*, string password*/)
     {
