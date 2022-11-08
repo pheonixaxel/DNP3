c1d0fecc934fce425252befde6815a6fb54fa4d2
Bhupas <bhupas@outlook.com>
2022-11-08 12:14:42 +0100

    added logic and stuff for subpost

--- Application/Logic/PostLogic.cs
+++ Application/Logic/PostLogic.cs
@@ -11,35 +11,27 @@ public class PostLogic : IPostLogic
     private readonly IPostDao postDao;
     private readonly IUserDao.IUserDao userDao;
 
-    public PostLogic(IPostDao postDao , IUserDao.IUserDao userDao)
+    public PostLogic(IPostDao postDao, IUserDao.IUserDao userDao)
     {
         this.postDao = postDao;
         this.userDao = userDao;
     }
-    
 
-    public Task<IEnumerable<Post>> GetCommentsAsync(Post parentPost)
+    public async Task<Post> CreateAsync(PostCreationDto postToCreate)
     {
-        throw new NotImplementedException();
-    }
-
-    public Task<Post> CreateAsync(PostCreationDto postCreationDto)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<Post>> GetAsync(string? subForm)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Post?> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DeleteAsync(int id)
-    {
-        throw new NotImplementedException();
+        User? user = await userDao.GetByIdAsync(postToCreate.OwnerId);
+        if (user == null)
+        {
+            throw new ArgumentException("User does not exist");
+        }
+        
+        Post post = new Post
+        {
+            Title = postToCreate.Title,
+            Content = postToCreate.Content,
+            OwnerId = user
+        };
+        Post created = await postDao.CreateAsync(post, SubPostCreationDto.id);
+        return created;
     }
 }
