4f06f76e3c8554c4972341e23c6e2f1ff2b7f173
Alex Toth <tothilian@gmail.com>
2022-11-09 17:23:18 +0100

    login2

---
+++ Blazor/Services/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Shared.Models;
+
+namespace Blazor.Services;
+
+public interface IAuthService
+{
+    Task LoginAsync(string username, string password);
+    Task LogoutAsync();
+
+    Task RegisterAsync(User user);
+    /*public Task<ClaimsPrincipal> GetAuthAsync();*/
+
+    Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+    Task<ClaimsPrincipal> GetAuthAsync();
+}
